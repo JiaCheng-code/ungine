f6509ae59ae7cdcfc6b940ca1395be5f7cf1e903
JiaCheng-code <675232630@qq.com>
2023-01-16 22:12:25 +0800

    feat:回到上一步事件完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,14 +31,17 @@ const navClose = () => {
 addEventOnElement(navLinks, "click", navClose)
 /*
 * header滚动超出选中事件
+* 回到上一步事件
 * */
 const header = document.querySelector("[data-header]");
-
+const backTopBtn = document.querySelector("[data-back-top-btn]")
 const activeEl = function (){
     if(window.scrollY>100){
         header.classList.add("active")
+        backTopBtn.classList.add("active")
     }else{
         header.classList.remove("active")
+        backTopBtn.classList.remove("active")
     }
 }
 window.addEventListener("scroll",activeEl)
